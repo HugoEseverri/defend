0263e55b0f997fb87cd5205d4200d5baf12680d1
Hugo Eseverri <hugooeseverri@gmail.com>
2025-05-02 18:18:15 -0300

    Colaboradores

---
+++ defend/src/components/Colaborators/Colaborators.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Image from 'next/image'
+
+function Colaborators() {
+    return (
+        <div>
+            <div>
+                <Image
+                    src="/alloc-1.png"
+                    alt='Colaboradores'
+                    width={30}
+                    height={30}
+                />
+                <h2>Colaboradores</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborators
